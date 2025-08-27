cbffad6019dafd0799204e358c09239e57b76d8d
rosstimo <rosstimo@isu.edu>
2025-08-27 08:47:27 -0600

    Add project files.

---
+++ ConsoleFun/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleFun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the first line!");
+            Console.WriteLine("Write the second line!");
+            Console.WriteLine("Write the third line!");
+            Console.WriteLine("Write the fourth line!");
+            Console.WriteLine("Whew!... I'm tired now");
+            Console.Read();
+        }
+    }
+}
